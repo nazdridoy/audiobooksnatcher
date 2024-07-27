19b384db147dc1074351243d55829e661193b2a4
nazDridoy <45564804+nazdridoy@users.noreply.github.com>
2024-07-27 17:16:56 +0600

    Refactor main.py
    - Add argparse for command-line source selection
    -  Questionary for interactive menu
    - Improve code structure

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-from pick import pick
+import questionary
 from src.chapters import get_chapters
 from src.download import get_input
 from src.search import search_book
 from os import system, name
+import argparse
 
-cls = lambda: system('cls' if name =='nt' else 'clear')
-_, idx = pick(['Search book', 'Download from URL', 'Exit'], 'Choose action:')
-options = [search_book, get_input, exit]
-res = options[idx]()
+cls = lambda: system('cls' if name == 'nt' else 'clear')
 
-if res: 
-	cls()
-	get_chapters(res)
+def main():
+    parser = argparse.ArgumentParser(description="Audio book downloader")
+    parser.add_argument("-S", "--source", choices=["toky", "freeaudiobooks"], default="toky", help="Source website")
+    args = parser.parse_args()
+
+    choices = [
+        'Search book',
+        'Download from URL',
+        'Exit'
+    ]
+
+    selected_action = questionary.select(
+        "Choose action:",
+        choices=choices
+    ).ask()
+
+    options = [
+        lambda: search_book(args.source),
+        lambda: get_input(args.source),
+        exit
+    ]
+    res = options[choices.index(selected_action)]()
+
+    if res:
+        cls()
+        get_chapters(res, args.source)
+
+if __name__ == "__main__":
+    main()
